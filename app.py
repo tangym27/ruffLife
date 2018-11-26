8ef8683291d98306c883bb6b8e0ca6c28e40e73c
Aaron Li <aaronli39@gmail.com>
2018-11-26 12:15:43 -0500

    did root route

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, request, sessions
+# Team ruffLife: Xiaojie(Aaron) Li, Michelle Tang, Bo Hui Lu, Kaitlin Wan
+
+from flask import Flask, request, render_template, session, url_for, redirect, flash
+
+import os
 
 # instantiate flask app
 app = Flask(__name__)
 
-# home, prompts user to log in if not logged in or 
-# redirects to webpage if logged in
+# generate random key
+app.secret_key = os.urandom(32)
+
+# root route
 @app.route("/")
-def home() :
-    return;
+def home():
+    """
+    If user is logged in, redirect them to their feed.
+    If not logged in, prompt login page
+    """
+
+    if "user" in session: 
+        return redirect("/profile")
+    return render_template("index.html")
+
+
 
 # run flask app with debug
 if __name__ == "__main__":
