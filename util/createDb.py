275113a232c170afbe9b01c0b2f3e8f1b43d46e3
Michelle Tang <mtang1@stuy.edu>
2018-11-28 12:01:25 -0500

    db

--- util/createDb.py
+++ util/createDb.py
@@ -11,28 +11,26 @@ c = db.cursor()
 
 '''user info table'''
 c.execute("CREATE TABLE IF NOT EXISTS userInfo(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT UNIQUE,
-pass TEXT,
-fav_Animal TEXT,
-fav_Meme TEXT)"
-)
-
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            pass TEXT,
+            fav_Animal TEXT,
+            fav_Meme TEXT)"
+            )
 
 '''quotes table'''
 c.execute("CREATE TABLE IF NOT EXISTS quotes(
-id INTEGER PRIMARY KEY AUTOINCREMENT, 
-name TEXT, 
-url TEXT)"
-)
-
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            url TEXT)"
+            )
 
 '''weather table'''
 c.execute("CREATE TABLE IF NOT EXISTS weather(
-day TEXT PRIMARY KEY, 
-temp TEXT, 
-condition TEXT)"
-)
+            day TEXT PRIMARY KEY,
+            temp TEXT,
+            condition TEXT)"
+            )
 
 db.commit()
 db.close()
