99532cb5b915e0f5d7548e45a571e56869ccee78
BoLu2019 <blu4@stuy.edu>
2018-11-29 12:08:44 -0500

    fixed createdb

--- util/createDb.py
+++ util/createDb.py
@@ -10,26 +10,26 @@ db = sqlite3.connect("../data/fluffy.db")
 c = db.cursor()
 
 '''user info table'''
-c.execute("CREATE TABLE IF NOT EXISTS userInfo(
+c.execute("""CREATE TABLE IF NOT EXISTS userInfo(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE,
             pass TEXT,
             fav_Animal TEXT,
-            fav_Meme TEXT)"
+            fav_Meme TEXT)"""
             )
 
 '''quotes table'''
-c.execute("CREATE TABLE IF NOT EXISTS quotes(
+c.execute("""CREATE TABLE IF NOT EXISTS quotes(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
-            url TEXT)"
+            url TEXT)"""
             )
 
 '''weather table'''
-c.execute("CREATE TABLE IF NOT EXISTS weather(
+c.execute("""CREATE TABLE IF NOT EXISTS weather(
             day TEXT PRIMARY KEY,
             temp TEXT,
-            condition TEXT)"
+            condition TEXT)"""
             )
 
 db.commit()
